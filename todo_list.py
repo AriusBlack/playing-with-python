69567a937679c455f818b8afdde7344d3d71944e
Armand J <34845373+AriusBlack@users.noreply.github.com>
2018-02-25 20:07:52 -0500

    Update todo_list.py

--- todo_list.py
+++ todo_list.py
@@ -18,17 +18,20 @@ def print_list():
 print_help()
 
 while True:
-#    try:    THIS IS NOT WORKING, I'm back to the same problem as with the original break. It can't seem to access the new_item creation
-#        new_item = input("> ")
-#    except NameError:
-#        print("Not the right format")
-    if new_item == 'HELP':
-        print_help()
-    elif new_item == 'DONE':
-        break
-    elif new_item == 'SHOW':
-        print_list()
-    elif new_item != 'SHOW''HELP''DONE':
-        todo_list.append(new_item)
-
+    try:
+        new_item = input("> ")
+        if new_item == 'HELP':
+            print_help()
+        elif new_item == 'DONE':
+            break
+        elif new_item == 'SHOW':
+            print_list()
+        elif new_item != 'SHOW''HELP''DONE':
+            todo_list.append(new_item)
+    except NameError:
+        pass
+        print("Not the right format")
+    except SyntaxError:
+        pass
+        print("Remember to open your entry with ' and close with '")
 print_list()
